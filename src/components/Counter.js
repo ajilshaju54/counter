0a44ff42587c1976ac1457513a5abad24a1ce8b0
ajilshaju54 <ajilshaju777@gmail.com>
2022-12-09 13:19:40 +0530

    counter

---
+++ src/components/Counter.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+
+import React from 'react'
+import './app.css'
+
+function Counter() {
+    const[counter,setCounter]=useState(0);
+
+   const increment=()=>{
+        setCounter(counter+1)
+    };
+
+   const decrement=()=>{
+        if(counter!==0){
+        setCounter(counter-1)
+   }};
+
+    const zero=()=>{
+        setCounter(0)
+    }
+
+  return (
+    <div>
+       <h4>COUNTER</h4>
+         <h2>The Number Is : {counter}</h2>
+         <button className="firstButton" onClick={increment}>increment</button>
+         <button className="secondButton" onClick={decrement}>decrement</button>
+         <button className="thirdButton" onClick={zero}>zero</button>
+    </div>
+  )
+}
+
+export default Counter
---
+++ src/components/Employee.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+function Employee() {
+
+const Employee=[
+    {username:'john',age:23},
+    {username:'joy',age:22},
+    {username:'ajil',age:23},
+    {username:'amal',age:21},
+]
+
+  return (
+    <div>
+      {
+        Employee.map(item=>(
+            <p>username:{item.username}
+            <br />
+            Age : {item.age}
+            </p>
+         ) )
+      }
+    </div>
+  )
+}
+
+export default Employee
